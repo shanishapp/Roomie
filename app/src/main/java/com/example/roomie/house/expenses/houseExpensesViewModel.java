f0c7cd1a33259baa8f8180ace3a461cb884029cb
uriga <uri.gaash@gmail.com>
2020-09-02 17:33:43 +0300

    uri

--- app/src/main/java/com/example/roomie/house/expenses/houseExpensesViewModel.java
+++ app/src/main/java/com/example/roomie/house/expenses/houseExpensesViewModel.java
@@ -6,15 +6,19 @@ import androidx.lifecycle.ViewModel;
 
 import com.example.roomie.FirestoreJob;
 import com.example.roomie.Roommate;
+import com.example.roomie.house.chores.allChoresJob;
+import com.example.roomie.house.chores.chore.Chore;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.example.roomie.util.FirestoreUtil.CHORESS_COLLECTION_NAME;
 import static com.example.roomie.util.FirestoreUtil.EXPENSES_COLLECTION_NAME;
 import static com.example.roomie.util.FirestoreUtil.HOUSES_COLLECTION_NAME;
 
-public class houseExpensesViewModel extends ViewModel
+public class houseExpensesViewModel extends ViewModel implements ExpenseAdapter.OnExpenseListener
 {
     private FirebaseFirestore db;
     private MutableLiveData<List<Expense>> expenses;
@@ -80,14 +84,48 @@ public class houseExpensesViewModel extends ViewModel
                     if (task.isSuccessful())
                     {
                         expenseJob.setJobStatus(FirestoreJob.JobStatus.SUCCESS);
-                        job.setValue(expenseJob);
                     } else
                     {
                         expenseJob.setJobStatus(FirestoreJob.JobStatus.ERROR);
                         expenseJob.setJobErrorCode(FirestoreJob.JobErrorCode.GENERAL);
-                        job.setValue(expenseJob);
                     }
+                    job.setValue(expenseJob);
                 });
         return job;
+    }
+
+    public LiveData<allExpensesJob> getExpenses(String houseId)
+    {
+        allExpensesJob expensesJob = new allExpensesJob(FirestoreJob.JobStatus.IN_PROGRESS);
+        MutableLiveData<allExpensesJob> job = new MutableLiveData<>(expensesJob);
+
+        db.collection(HOUSES_COLLECTION_NAME)
+                .document(houseId).collection(EXPENSES_COLLECTION_NAME)
+                .get().addOnCompleteListener(task -> {
+            if (task.isSuccessful())
+            {
+                List<Expense> fetchedList = new ArrayList<>();
+                for (DocumentSnapshot documentSnapshot : task.getResult())
+                {
+                    Expense expense = documentSnapshot.toObject(Expense.class);
+                    fetchedList.add(expense);
+                }
+                expenses.setValue(fetchedList);
+                expensesJob.setExpenses(fetchedList);
+                expensesJob.setJobStatus(FirestoreJob.JobStatus.SUCCESS);
+            } else
+            {
+                expensesJob.setJobStatus(FirestoreJob.JobStatus.ERROR);
+                expensesJob.setJobErrorCode(FirestoreJob.JobErrorCode.GENERAL);
+            }
+            job.setValue(expensesJob);
+        });
+        return job;
+    }
+
+    @Override
+    public void onExpenseClick(int pos)
+    {
+        //TODO: implement this
     }
 }
