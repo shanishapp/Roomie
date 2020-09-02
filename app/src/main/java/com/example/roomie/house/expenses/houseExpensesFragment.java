120c26b8890ba1dbfe12345a3a9b8c7bcab23a03
uriga <uri.gaash@gmail.com>
2020-09-02 17:28:22 +0300

    uri

--- app/src/main/java/com/example/roomie/house/expenses/houseExpensesFragment.java
+++ app/src/main/java/com/example/roomie/house/expenses/houseExpensesFragment.java
@@ -3,21 +3,32 @@ package com.example.roomie.house.expenses;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.roomie.MovableFloatingActionButton;
 import com.example.roomie.R;
+import com.example.roomie.house.HouseActivityViewModel;
+import com.example.roomie.house.chores.HouseChoresFragmentViewModel;
+import com.example.roomie.house.chores.chore.Chore;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link houseExpensesFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class houseExpensesFragment extends Fragment
+public class houseExpensesFragment extends Fragment implements ExpenseAdapter.OnExpenseListener
 {
+
+
+
 
     public houseExpensesFragment()
     {
@@ -47,6 +58,12 @@ public class houseExpensesFragment extends Fragment
     {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_house_expenses, container, false);
+    }
+
+    @Override
+    public void onExpenseClick(int pos)
+    {
+
     }
 
     //TODO: need a view for total
--- app/src/main/java/com/example/roomie/house/expenses/houseExpensesViewModel.java
+++ app/src/main/java/com/example/roomie/house/expenses/houseExpensesViewModel.java
@@ -4,10 +4,15 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.roomie.FirestoreJob;
+import com.example.roomie.Roommate;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.example.roomie.util.FirestoreUtil.EXPENSES_COLLECTION_NAME;
+import static com.example.roomie.util.FirestoreUtil.HOUSES_COLLECTION_NAME;
 
 public class houseExpensesViewModel extends ViewModel
 {
@@ -21,9 +26,68 @@ public class houseExpensesViewModel extends ViewModel
         expenses = new MutableLiveData<>(new ArrayList<>());
     }
 
-    public LiveData<CreateNewExpenseJob> setRoomie(String expenseId, String roomie, String houseId)
+    public LiveData<CreateNewExpenseJob> setPayer(String expenseId, Roommate payer, String houseId)
     {
-        //TODO: only started
-        return null;
+        CreateNewExpenseJob expensesJob = new CreateNewExpenseJob(FirestoreJob.JobStatus.IN_PROGRESS);
+        MutableLiveData<CreateNewExpenseJob> job = new MutableLiveData<>(expensesJob);
+
+        db.collection(HOUSES_COLLECTION_NAME)
+                .document(houseId).collection(EXPENSES_COLLECTION_NAME)
+                .document(expenseId)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful())
+                    {
+                        Expense expense = task.getResult().toObject(Expense.class);
+                        if (expense != null)
+                        {
+                            List<Expense> expenseList = expenses.getValue();
+                            assert expenseList != null;
+                            expenseList.remove(expense);
+                            expense.set_payer(payer);
+                            expenseList.add(expense);
+                            expenses.setValue(expenseList);
+                            db.collection(HOUSES_COLLECTION_NAME)
+                                    .document(houseId).collection(EXPENSES_COLLECTION_NAME)
+                                    .document(expenseId).update("payer", payer);
+
+                            expensesJob.setExpense(task.getResult().toObject(Expense.class));
+                            expensesJob.setJobStatus(FirestoreJob.JobStatus.SUCCESS);
+                            job.setValue(expensesJob);
+                        }
+                    } else
+                    {
+                        expensesJob.setJobStatus(FirestoreJob.JobStatus.ERROR);
+                        expensesJob.setJobErrorCode(FirestoreJob.JobErrorCode.GENERAL);
+                        job.setValue(expensesJob);
+                    }
+                });
+
+        return job;
+    }
+
+    public LiveData<CreateNewExpenseJob> deleteExpense(Expense expense, String houseId)
+    {
+
+        CreateNewExpenseJob expenseJob = new CreateNewExpenseJob(FirestoreJob.JobStatus.IN_PROGRESS);
+        MutableLiveData<CreateNewExpenseJob> job = new MutableLiveData<>(expenseJob);
+
+        expenses.getValue().remove(expense); // TODO does it work ?
+        db.collection(HOUSES_COLLECTION_NAME)
+                .document(houseId).collection(EXPENSES_COLLECTION_NAME)
+                .document(expense.get_id()).delete()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful())
+                    {
+                        expenseJob.setJobStatus(FirestoreJob.JobStatus.SUCCESS);
+                        job.setValue(expenseJob);
+                    } else
+                    {
+                        expenseJob.setJobStatus(FirestoreJob.JobStatus.ERROR);
+                        expenseJob.setJobErrorCode(FirestoreJob.JobErrorCode.GENERAL);
+                        job.setValue(expenseJob);
+                    }
+                });
+        return job;
     }
 }
