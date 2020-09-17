d36fbe74bde67f00799d85ca9aabff349d4c0b90
shani shapp <shanishapp@gmail.com>
2020-09-17 18:22:30 +0300

    Chore: fixups

--- app/src/main/java/com/example/roomie/house/chores/HouseChoresFragment.java
+++ app/src/main/java/com/example/roomie/house/chores/HouseChoresFragment.java
@@ -388,6 +388,9 @@ public class HouseChoresFragment extends Fragment implements ChoreAdapter.OnChor
         LiveData<newChoreJob> job = vm.setDone(chore, !chore.is_choreDone(), houseActivityViewModel.getHouse().getId());
         job.observe(getViewLifecycleOwner(), newChoreJob -> {
             if(newChoreJob.getJobStatus() == FirestoreJob.JobStatus.SUCCESS) {
+                if(chore.is_choreDone()) {
+                    choreList.remove(chore);
+                }
                 adapter.notifyDataSetChanged();
                 adapter.closeMenu();
             }
