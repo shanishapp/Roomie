d1ca6f6132af3f6d6108eab6b09e9232bbd11f72
shani shapp <shanishapp@gmail.com>
2020-10-13 13:19:04 +0300

    Groceries: documentation

--- app/src/main/java/com/roomiemain/roomie/house/groceries/HouseGroceriesFragment.java
+++ app/src/main/java/com/roomiemain/roomie/house/groceries/HouseGroceriesFragment.java
@@ -54,7 +54,7 @@ public class HouseGroceriesFragment extends Fragment implements GroceryAdapter.O
     private HouseActivityViewModel houseActivityViewModel;
     private HouseGroceriesFragmentViewModel vm;
     private ArrayList<Grocery> groceryList;
-    private MovableFloatingActionButton addChoreButton;
+    private MovableFloatingActionButton addGroceryButton;
     private MovableFloatingActionButton moveToExpensesBtn;
     private NavController navController;
     private FrameLayout loadingOverlay;
@@ -65,6 +65,7 @@ public class HouseGroceriesFragment extends Fragment implements GroceryAdapter.O
         // Required empty public constructor
     }
 
+    /* create view */
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -100,6 +101,38 @@ public class HouseGroceriesFragment extends Fragment implements GroceryAdapter.O
         return v;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        addGroceryButton = view.findViewById(R.id.fab);
+        moveToExpensesBtn = view.findViewById(R.id.addToExpensesBtn);
+        pickedGroceries = new ArrayList<>();
+        //set up add button_bg_grey
+        addGroceryButton.setOnClickListener(view1 -> {
+            if(view1 != null){
+                navController.navigate(R.id.action_house_groceries_fragment_dest_to_newGroceryFragment);
+            }
+        });
+
+        setMoveToExpenseButton();
+        loadingOverlay = view.findViewById(R.id.groceries_loading_overlay);
+        toggleLoadingOverlay(true);
+    }
+
+    /* ui */
+
+    private void toggleLoadingOverlay(boolean isVisible) {
+        if (isVisible) {
+            loadingOverlay.setVisibility(View.VISIBLE);
+        } else {
+            loadingOverlay.setVisibility(View.GONE);
+        }
+    }
+
+
+    /* recyclerview*/
+
     private void setRecyclerView(View v) {
         recyclerView = v.findViewById(R.id.groceriesRecyclerView);
         layoutManager = new LinearLayoutManagerWithSmoothScroller(getContext());
@@ -113,15 +146,6 @@ public class HouseGroceriesFragment extends Fragment implements GroceryAdapter.O
         adapter = new GroceryAdapter(groceryList, HouseGroceriesFragment.this);
         recyclerView.setAdapter(adapter);
     }
-
-    private void toggleLoadingOverlay(boolean isVisible) {
-        if (isVisible) {
-            loadingOverlay.setVisibility(View.VISIBLE);
-        } else {
-            loadingOverlay.setVisibility(View.GONE);
-        }
-    }
-
 
     @Override
     public boolean onGroceryClick(int pos, CustomCheckBox pickGroceryCheckBox) {
@@ -168,54 +192,47 @@ public class HouseGroceriesFragment extends Fragment implements GroceryAdapter.O
     }
 
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        navController = Navigation.findNavController(view);
-        addChoreButton = view.findViewById(R.id.fab);
-        moveToExpensesBtn = view.findViewById(R.id.addToExpensesBtn);
-        pickedGroceries = new ArrayList<>();
-        //set up add button_bg_grey
-        addChoreButton.setOnClickListener(view1 -> {
-            if(view1 != null){
-                navController.navigate(R.id.action_house_groceries_fragment_dest_to_newGroceryFragment);
-            }
-        });
-
+    /* move to expense functionalities*/
+    private void setMoveToExpenseButton() {
         moveToExpensesBtn.setOnClickListener(view12 -> {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                    AlertDialog alertDialog = builder.create();
-                    final View customLayout = getLayoutInflater().inflate(R.layout.dialog_create_grocery_expense, null);
-
-                    ((TextView)customLayout.findViewById(R.id.chosenGroceriesCount)).setText(String.valueOf(pickedGroceries.size()));
-                    customLayout.findViewById(R.id.doAddToExpensesBtn).setOnClickListener(view1 -> {
-                        String description = getString(R.string.house_groceries_fragment_title);
-
-                        NewExpenseViewModel expenseViewModel = new ViewModelProvider(requireActivity()).get(NewExpenseViewModel.class);
-                        expenseViewModel.createNewExpense(houseActivityViewModel.getHouse(),
-                                getString(R.string.house_bottom_menu_groceries), description,
-                                Integer.parseInt(((EditText)customLayout.findViewById(R.id.editTextNumber)).getText().toString()),
-                                user.getUid(), user.getDisplayName(), Expense.ExpenseType.GROCERIES);
-
-                        for (Grocery grocery : pickedGroceries) {
-                            vm.deleteGroceryForever(grocery, houseActivityViewModel.getHouse().getId());
-                            groceryList.remove(grocery);
-                        }
-                        pickedGroceries.clear();
-                        moveToExpensesBtn.setVisibility(View.GONE);
-                        adapter.notifyDataSetChanged();
-                        alertDialog.cancel();
-                    });
-                    customLayout.findViewById(R.id.cancelBtn).setOnClickListener(view13 -> {
-                        alertDialog.cancel();
-                    });
-
-                    alertDialog.setView(customLayout);
-                    alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                    alertDialog.show();
-                });
-        loadingOverlay = view.findViewById(R.id.groceries_loading_overlay);
-        toggleLoadingOverlay(true);
+            showMoveToExpenseDialog();
+        });
     }
 
+    private void showMoveToExpenseDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        AlertDialog alertDialog = builder.create();
+        final View customLayout = getLayoutInflater().inflate(R.layout.dialog_create_grocery_expense, null);
+
+        ((TextView)customLayout.findViewById(R.id.chosenGroceriesCount)).setText(String.valueOf(pickedGroceries.size()));
+        setMoveToExpenseDialogButton(alertDialog, customLayout);
+        customLayout.findViewById(R.id.cancelBtn).setOnClickListener(view13 -> {
+            alertDialog.cancel();
+        });
+
+        alertDialog.setView(customLayout);
+        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        alertDialog.show();
+    }
+
+    private void setMoveToExpenseDialogButton(AlertDialog alertDialog, View customLayout) {
+        customLayout.findViewById(R.id.doAddToExpensesBtn).setOnClickListener(view1 -> {
+            String description = getString(R.string.house_groceries_fragment_title);
+
+            NewExpenseViewModel expenseViewModel = new ViewModelProvider(requireActivity()).get(NewExpenseViewModel.class);
+            expenseViewModel.createNewExpense(houseActivityViewModel.getHouse(),
+                    getString(R.string.house_bottom_menu_groceries), description,
+                    Integer.parseInt(((EditText)customLayout.findViewById(R.id.editTextNumber)).getText().toString()),
+                    user.getUid(), user.getDisplayName(), Expense.ExpenseType.GROCERIES);
+
+            for (Grocery grocery : pickedGroceries) {
+                vm.deleteGroceryForever(grocery, houseActivityViewModel.getHouse().getId());
+                groceryList.remove(grocery);
+            }
+            pickedGroceries.clear();
+            moveToExpensesBtn.setVisibility(View.GONE);
+            adapter.notifyDataSetChanged();
+            alertDialog.cancel();
+        });
+    }
 }
